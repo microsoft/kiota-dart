6b3414a64b43bdb0aa87b727a76bf9173e2e5e76
Vincent Biret <vibiret@microsoft.com>
2025-03-03 13:32:48 -0500

    chore: base package scaffolding
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

---
+++ packages/microsoft_kiota_azure/lib/microsoft_kiota_azure.dart
@@ -0,0 +1,10 @@
+﻿/// This library implements an Azure authentication provider for the
+/// [`microsoft_kiota_abstractions`](https://pub.dev/packages/microsoft_kiota_abstractions) package.
+library microsoft_kiota_azure;
+
+import 'package:microsoft_kiota_abstractions/microsoft_kiota_abstractions.dart';
+import 'package:oauth2/oauth2.dart' as oauth2;
+
+part 'src/azure_access_token_provider.dart';
+part 'src/azure_authentication_provider.dart';
+//TODO add the other parts of the model
