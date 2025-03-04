b42b5a92acebbf2cba03305f96fe3f247849185e
Vincent Biret <vibiret@microsoft.com>
2025-03-04 14:13:36 -0500

    chore: adds tests for token deserialization
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

---
+++ packages/microsoft_kiota_azure/test/device_code_token_response_test.dart
@@ -0,0 +1,25 @@
+import 'package:microsoft_kiota_azure/microsoft_kiota_azure.dart';
+import 'package:test/test.dart';
+import 'dart:convert';
+
+void main() async {
+	test('deserializes device token error from json payload', () async {
+		final String json = '''
+		{
+			"token_type": "Bearer",
+			"scope": "User.Read profile openid email",
+			"expires_in": 3599,
+			"access_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ik5HVEZ2ZEstZnl0aEV1Q...",
+			"refresh_token": "AwABAAAAvPM1KaPlrEqdFSBzjqfTGAMxZGUTdM0t4B4...",
+			"id_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJub25lIn0.eyJhdWQiOiIyZDRkMTFhMi1mODE0LTQ2YTctOD..."
+		}
+		''';
+		final deviceCodeTokenResponse = DeviceCodeTokenResponse.fromJson(jsonDecode(json));
+		expect(deviceCodeTokenResponse.accessToken, 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ik5HVEZ2ZEstZnl0aEV1Q...');
+		expect(deviceCodeTokenResponse.refreshToken, 'AwABAAAAvPM1KaPlrEqdFSBzjqfTGAMxZGUTdM0t4B4...');
+		expect(deviceCodeTokenResponse.idToken, 'eyJ0eXAiOiJKV1QiLCJhbGciOiJub25lIn0.eyJhdWQiOiIyZDRkMTFhMi1mODE0LTQ2YTctOD...');
+		expect(deviceCodeTokenResponse.expiresIn, 3599);
+		expect(deviceCodeTokenResponse.tokenType, 'Bearer');
+		expect(deviceCodeTokenResponse.scope, 'User.Read profile openid email');
+	});
+}
