5eaa3155a6d4f6af40d00742f3f6e33e47996253
Vincent Biret <vibiret@microsoft.com>
2026-03-17 10:02:08 -0400

    Revert "chore: reverts uuid parsing to align with initial behaviour"
    This reverts commit 43b2919596c5e9afb5b94697dfc95c55a943e7bc.

--- packages/microsoft_kiota_serialization_json/lib/src/json_parse_node.dart
+++ packages/microsoft_kiota_serialization_json/lib/src/json_parse_node.dart
@@ -182,7 +182,7 @@ class JsonParseNode implements ParseNode {
   // ignore: experimental_member_use
   static UuidValue? _parseGuidValue(String value) {
     // ignore: experimental_member_use
-    return UuidValue.raw(value);
+    return UuidValue.withValidation(value);
   }
 
   static TimeOnly? _parseTimeOnlyValue(String value) =>
